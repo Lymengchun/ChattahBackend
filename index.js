c815e1d59fae69c1b16fdfd83e008720ec8a2e1a
Lymeng <lymengchun@gmail.com>
2022-01-13 13:56:10 +0700

    first commit

---
+++ index.js
@@ -0,0 +1,24 @@
+const express = require('express');
+const app = express();
+
+const port = process.env.PORT || 4000;
+
+const server = app.listen(port,()=>{
+  console.log('server is Started on',port);
+});
+
+
+const io = require('socket.io')(server);
+
+//initialize socket
+io.on('connection',(socket)=>{
+  console.log("socket connected",socket.id);
+  socket.on("disconnect",()=>{
+    console.log("socket disconnected",socket.id);
+  });
+
+  socket.on('test',(msg)=>{
+    console.log(msg);
+  })
+});
+
