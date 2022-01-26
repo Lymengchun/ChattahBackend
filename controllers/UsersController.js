5bc2e9a178870faa393acc6f871664ccbb8d0d84
Lymeng <lymengchun@gmail.com>
2022-01-26 14:14:17 +0700

    crud auth

--- controllers/UsersController.js
+++ controllers/UsersController.js
@@ -72,101 +72,101 @@ module.exports = {
     register,login
 }
 
-// // Show the list of users
-// const index = (req, res, next) => {
-//      Users.find()
-//      .then(response => {
-//          res.json({
-//              response
-//          })
-//      })
-//      .catch(errors => {
-//          res.json({
-//              message: 'An error Occured!'
-//          })
-//      })
-// };
+// Show the list of users
+const index = (req, res, next) => {
+     Users.find()
+     .then(response => {
+         res.json({
+             response
+         })
+     })
+     .catch(errors => {
+         res.json({
+             message: 'An error Occured!'
+         })
+     })
+};
 
-// // Show single User
-// const show = (req, res, next) => {
-//     let userID = req.body.userID;
+// Show single User
+const show = (req, res, next) => {
+    let userID = req.body.userID;
 
-//     Users.findById(userID)
-//     .then(response => {
-//         res.json({
-//             response
-//         })
-//     })
+    Users.findById(userID)
+    .then(response => {
+        res.json({
+            response
+        })
+    })
 
-//     .catch(error => {
-//         res.json({
-//             message: 'An error Occured!'
-//         })
-//     })
-// }
+    .catch(error => {
+        res.json({
+            message: 'An error Occured!'
+        })
+    })
+}
 
-// // add new users
-// const store = (req, res, next) => {
-//     let users = new Users({
-//                     name: req.body.name,
-//                     email:req.body.facebookID,
-//                     image: req.body.image
-//     })
+// add new users
+const store = (req, res, next) => {
+    let users = new Users({
+                    name: req.body.name,
+                    email:req.body.facebookID,
+                    image: req.body.image
+    })
    
-//         users.save()
-//         .then(response => {
-//             res.json({
-//                 message: 'Users Added Successfully!'
-//             })
-//         })
-//         .catch(error => {
-//             res.json({
-//                 message: 'An error Occured!'
-//             })
-//         })
+        users.save()
+        .then(response => {
+            res.json({
+                message: 'Users Added Successfully!'
+            })
+        })
+        .catch(error => {
+            res.json({
+                message: 'An error Occured!'
+            })
+        })
 
 
-// }
+}
 
-// // update an users
-// const update = (req, res, next) => {
-//     let userID = req.body.userID;
+// update an users
+const update = (req, res, next) => {
+    let userID = req.body.userID;
 
-//     let updatedData = {
-//         name: req.body.name,
-//         facebookID: req.body.facebookID,
-//         image: req.body.image
-//     }
+    let updatedData = {
+        name: req.body.name,
+        facebookID: req.body.facebookID,
+        image: req.body.image
+    }
 
-//     Users.findByIdAndUpdate(userID, {$set: updatedData})
-//     .then(() => {
-//         res.json({
-//                 message: 'User Updated Successfully!'
-//         })
-//     })
-//     .catch(error => {
-//         res.json({
-//             message: 'An error Occured!'
-//         })
-//     })
-// }
+    Users.findByIdAndUpdate(userID, {$set: updatedData})
+    .then(() => {
+        res.json({
+                message: 'User Updated Successfully!'
+        })
+    })
+    .catch(error => {
+        res.json({
+            message: 'An error Occured!'
+        })
+    })
+}
 
-// // delete an Users
-//  const destroy = (req, res, next) => {
-//      let userID = req.body.userID
-//      Users.findByIdAndRemove(userID)
-//      .then(() => {
-//          req.json({
-//              message: 'User deleted successfully!'
-//          })
-//      })
-//      .catch(error => {
-//          req.json({
-//              message: 'An error Occured!'
-//          })
-//      })
-//  }
+// delete an Users
+ const destroy = (req, res, next) => {
+     let userID = req.body.userID
+     Users.findByIdAndRemove(userID)
+     .then(() => {
+         req.json({
+             message: 'User deleted successfully!'
+         })
+     })
+     .catch(error => {
+         req.json({
+             message: 'An error Occured!'
+         })
+     })
+ }
 
-//  module.exports = {
-//      index, show, store, update, destroy
-//  }
+ module.exports = {
+     index, show, store, update, destroy
+ }
--- routes/auth.js
+++ routes/auth.js
@@ -2,8 +2,15 @@ const express = require('express')
 const router = express.Router()
 
 const AuthController = require('../controllers/UsersController')
+const authenticate = require('../middleware/authenticate')
 
 router.post('/register',AuthController.register)
 router.post('/login',AuthController.login)
+
+router.get('/',authenticate,AuthController.index);
+router.post('/show',authenticate, AuthController.show);
+router.post('/store',authenticate,AuthController.store);
+router.post('/update',authenticate,AuthController.update);
+router.post('/delete',authenticate,AuthController.destroy);
 
 module.exports = router
