08d528b20a88075fe68acf3258c55f520abd0010
Lymeng <lymengchun@gmail.com>
2022-02-08 11:41:34 +0700

    change post to put in push message

--- routes/chatRounte.js
+++ routes/chatRounte.js
@@ -9,6 +9,6 @@ router.post('/show',authenticate, chatController.show);
 router.post('/store',authenticate,chatController.store);
 router.post('/update',authenticate,chatController.update);
 router.post('/delete',authenticate,chatController.destroy);
-router.post('/pushMessages',authenticate,chatController.pushMessage);
+router.put('/pushMessages',authenticate,chatController.pushMessage);
 
 module.exports = router;
