3be63ea67731fc8b0ca86c868219cfc934c80beb
Lymeng <lymengchun@gmail.com>
2022-02-08 11:36:15 +0700

    add rounte

--- routes/chatRounte.js
+++ routes/chatRounte.js
@@ -9,5 +9,6 @@ router.post('/show',authenticate, chatController.show);
 router.post('/store',authenticate,chatController.store);
 router.post('/update',authenticate,chatController.update);
 router.post('/delete',authenticate,chatController.destroy);
+router.post('/pushMessages',authenticate,chatController.pushMessage);
 
 module.exports = router;
