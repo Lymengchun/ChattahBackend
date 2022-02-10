8945d4aac54cf85f3017617ea6633d527f8fb3e0
Lymeng <lymengchun@gmail.com>
2022-02-10 08:54:09 +0700

    change to post

--- routes/chatRounte.js
+++ routes/chatRounte.js
@@ -5,7 +5,7 @@ const chatController = require('../controllers/ChatController');
 const authenticate = require('../middleware/authenticate')
 
 router.get('/',authenticate,chatController.index);
-router.get('/getchat',authenticate,chatController.getchat);
+router.post('/getchat',authenticate,chatController.getchat);
 router.post('/show',authenticate, chatController.show);
 router.post('/store',authenticate,chatController.store);
 router.post('/update',authenticate,chatController.update);
