19064ac89893653b93fe08345d75252ac2285cef
Lymeng <lymengchun@gmail.com>
2022-02-10 13:59:13 +0700

    fixed

--- controllers/ChatController.js
+++ controllers/ChatController.js
@@ -111,7 +111,7 @@ const getownchat = (req,res,next) => {
     chat.find({$or:[{sourceId:req.body.sourceId},{targetId:req.body.sourceId}]})
     .then(response =>{
         res.json({
-            response:response[0],
+            response:response,
             message: 'get own chat list successfully!'
         })
 
